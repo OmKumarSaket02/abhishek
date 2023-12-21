b5be04aaa53da6ff511e10d21117abed8970dc20
Om Kumar Saket <124894897+OmKumarSaket02@users.noreply.github.com>
2023-12-21 16:53:55 +0000

    01 class basics javascript

---
+++ 01_basics.js
@@ -0,0 +1,15 @@
+// variable -> variable is nothing 
+
+const accountId = 14521
+let accountEmail = "omkumarsaket86@gmail.com"
+var accountPassword = '321655'
+accountCity = "jaypur"
+
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "1212121221"
+accountCity = "Banguluru"
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity ])
+
